c0bf6153fee570bcecfdd44d08d79ec82a174674
staracek <staracek@foundation.sk>
2021-02-15 17:57:17 +0100

    base layout implemented

--- app/src/main/java/sk/lubostar/bignerdguide/bignerdguide_nerdlauncher/MainActivity.kt
+++ app/src/main/java/sk/lubostar/bignerdguide/bignerdguide_nerdlauncher/MainActivity.kt
@@ -2,10 +2,13 @@ package sk.lubostar.bignerdguide.bignerdguide_nerdlauncher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_nerd_launcher.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_nerd_launcher)
+        app_recycler_view.layoutManager = LinearLayoutManager(this)
     }
 }
